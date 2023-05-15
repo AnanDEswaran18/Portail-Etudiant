f2433056ae12a09e398da31d99e34b7fb99f5101
AnanDEswaran18 <bagavathianandhan002@gmail.com>
2023-05-15 21:28:09 +0530

    commit: .env

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const testRoutes = require("./routes/test")
 const app = express()
 
 // db
-/*
+
 mongoose
     .connect(process.env.MONGO_URI,{
         useNewUrlParser:true,
@@ -22,7 +22,7 @@ mongoose
     .catch((error)=>{
         console.log("DB Connection Error")
     })
-*/
+
 // middleware
 app.use(morgan("dev"))
 app.use(cors({ origin: true, credentials: true }))
