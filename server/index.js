13e8660ddde2deb57ca7cfa7ac1cc031bfaafc08
AnanDEswaran18 <bagavathianandhan002@gmail.com>
2023-05-22 18:14:50 +0530

    mongodb: Schema, inserting-data, retriving-data

--- server/index.js
+++ server/index.js
@@ -10,7 +10,6 @@ const testRoutes = require("./routes/test")
 const app = express()
 
 // db
-
 mongoose
     .connect(process.env.MONGO_URI,{
         useNewUrlParser:true,
@@ -20,8 +19,44 @@ mongoose
         console.log("DB CONNECTED")
     })
     .catch((error)=>{
-        console.log("DB Connection Error",error)
+        console.log("DB Connection Error - ",error)
     })
+
+const Schema = mongoose.Schema;
+
+const dataSchema = new Schema({
+    name: String,
+    age: Number,
+});
+
+const DataModel = mongoose.model('Data', dataSchema);
+
+async function insertData() {
+    try {
+        const newData = new DataModel({ name: 'John', age: 25 });
+        await newData.save();
+        console.log('Data inserted successfully');
+    } catch (error) {
+        console.error('Error inserting data', error);
+    }
+}
+
+async function retrieveData() {
+    try {
+        const data = await DataModel.find();
+        const names = data.map((item) => item.name);
+        console.log('Retrieved data:',names);
+    } catch (error) {
+        console.error('Error fetching data', error);
+    }
+}
+
+async function run() {
+    // await insertData();
+    await retrieveData();
+}
+
+run();
 
 // middleware
 app.use(morgan("dev"))
